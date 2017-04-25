cc1b8c9751f6669968def0a77210fb97df6dda1f
Gene Hallman <genehallman@gmail.com>
2017-04-25 02:16:55 -0700

    missed rename var

--- src/addon.cc
+++ src/addon.cc
@@ -1,13 +1,13 @@
 //#define BUILDING_NODE_EXTENSION
 #include <nan.h>
 
-#include "dbstore.h"
+#include "db.h"
 #include "dbenv.h"
 
 using namespace v8;
 
 void Init(Local<Object> exports) {
-  DbStore::Init(exports);
+  Db::Init(exports);
   DbEnv::Init(exports);
 }
 
